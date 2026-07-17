2cd351a403b1bffb8d5175655834dea073be54bc
adolfjeremy <adolf.jer@gmail.com>
2026-07-17 15:51:46 +0700

    chore: update admin seeding script and regenerate build artifacts

--- backend/dist/seed-admin.js
+++ backend/dist/seed-admin.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
-const prisma = new client_1.PrismaClient();
+const prisma = new client_1.PrismaClient({
+    log: ['info', 'query'],
+});
 const bcrypt = require("bcrypt");
 async function main() {
     const adminEmail = 'jeremy@pixby.id';
